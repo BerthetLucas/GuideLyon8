f83336c687b1cb952d59fbd83886804256991116
Lucas Berthet <lu.berthet@gmail.com>
2023-06-14 19:59:58 +0200

    ajout de la photo sur page principale

--- src/components/illu.js
+++ src/components/illu.js
@@ -1,9 +1,19 @@
 import imageDeLaPlace from '../assets/Monplaisir.jpg'
+import '../styles/illu.css'
+
 
 function BasDePagePrincipale (){
 
 return (
+    <div className='main'>
+
+    <div className='containeralpha'>
+
+
     <img src={imageDeLaPlace} className='imagePlace' alt='vue aerienne de la place monplaisir'></img>
+    
+    </div>
+    </div>
 )
 
 }
